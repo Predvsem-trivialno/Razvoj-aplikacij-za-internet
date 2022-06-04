3939c3c7b3e2d7623cfb8c397b21de9712fed8c5
xmapo <matejp@yahoo.com>
2022-06-04 21:58:47 +0200

    Added file limit to app.js

--- app.js
+++ app.js
@@ -93,6 +93,9 @@ app.use('/postbox', postboxRouter);
 app.use('/accesslog', accesslogRouter);
 app.use('/token', tokenRouter);
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
