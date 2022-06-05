20a42a7841d71eec9ed2439a57c14b6cb7e822da
xmapo <matejp@yahoo.com>
2022-06-05 11:08:37 +0200

    Testing python script

--- public/python/login.py
+++ public/python/login.py
@@ -11,31 +11,4 @@ def fileToObj(dir):
     with open(dir, 'rb') as handle:
         return pickle.load(handle)
 
-dirname = os.path.dirname(os.path.abspath(__file__))
-modelsFolder = os.path.join(dirname,'Models')
-
-mlp = fileToObj(modelsFolder+"/model.pickle")
-img = fr.decodeString(sys.argv[0])
-img = fr.getFace(img)
-
-lbp_hog = []
-
-gradients, directions = fr.sobel(img)
-imgLbp = fr.lbp(img).tolist()
-imgHog = fr.hog(img,8,12,2,gradients,directions)
-join = imgLbp+imgHog
-lbp_hog.append(join)
-
-prediction = mlp.predict_proba(lbp_hog)
-
-index = 0
-max = 0
-for i in range(0,len(mlp.classes_-1)):
-    if(prediction[i]>max):
-        index=i
-        max=prediction[i]
-
-if(max>60):
-    print(mlp.classes_[index],flush=True)
-else:
-    print("unknown",flush=True)
+print(sys.argv[0],flush=True)
